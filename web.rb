1599667f488d3e4d02b108a90e9e08eb245d7e0b
Gonzalo <gonzalo.mestre.leiin@gmail.com>
2015-12-03 11:58:10 +0100

    initial commit

---
+++ web.rb
@@ -0,0 +1,28 @@
+require "sinatra"
+require "pony"
+get "/index" do
+  erb :index
+end
+get "/catalogo" do
+  erb :catalogo
+end
+get "/historia" do
+  erb :historia
+end
+get "/contacto" do
+  erb :contacto
+end
+post"/send" do
+Pony.mail(to:"infosheedo@gmail.com",
+from:params[:email],
+body: "#{params[:name]}
+#{params[:message]}"
+subject:"formulario de contcto",
+via: :smtp, via_options:{adress:"smtp.gmail",
+  port:587,
+  enable_starttls_auto:true,
+  user_name:"infosheedo@gmail.com",
+  password:"",
+  authentication:"login"})
+  redirect("/contacto")
+end
